5a006ed387eabbb88799a58a3230dc934ac66a02
sugi-01096 <nodoguro04@gmail.com>
2024-12-27 10:10:35 +0900

    1

--- main.py
+++ main.py
@@ -6,3 +6,18 @@ import pytz
 import urllib.parse
 st.title('素数判定')
 st.markdown("自然数を入力してください")
+n= input()
+sosuhantei=1
+i=2
+num=n
+for num in i+1:
+    if n%i==0:
+        sosuhantei=0
+        i=num
+    i=i+1
+if n==1:
+    st.write(n+"は素数ではありません")
+elif sosuhantei ==1:
+    st.write(n+"は素数です")
+else:
+   st.write(n+"は素数ではありません")
