119071f3f73674cbe98d6b5692937ee4a42e972b
sugi-01096 <nodoguro04@gmail.com>
2024-12-27 11:46:58 +0900

    a

--- kosu.py
+++ kosu.py
@@ -3,27 +3,34 @@ import streamlit as st
 st.title('素数個数判定')
 st.markdown("2以上の自然数を入力してください")
 n = st.number_input("自然数", value=2, step=1)
-prime[100]=[2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-j=1
-counter=0
-num=3
-while num<=n:
-    sosuhantei=1
-    i=1
-    while i<j and sosuhantei ==1 and print[i]*print[i]:
-        counter=counter+1
-        if num%prime[i]==0:
-            sosuhantei=0
-        i=i+1
-        if sosuhantei==1:
-            prime[j]=2
-            j=j+1
-        num=num+2
-st.write("１から"+n+"までの素数")
-i=0
-while prime[i]!=0:
-    st.write (prime[i])
-    i=i+1
-st.write("割り算を行った回数："+counter)
-    
-        
+
+# 最大値nに対して、素数のリストを初期化
+prime = [0] * 100  # 素数を格納するリスト（サイズ100で初期化）
+prime[0] = 2  # 最初の素数2を格納
+j = 1  # 素数リストのインデックス
+counter = 0  # 割り算回数のカウンター
+num = 3  # 次にチェックする数字
+
+# 素数判定のループ
+while num <= n:
+    sosuhantei = 1  # 素数かどうかのフラグ
+    i = 0  # 素数リストのインデックス
+    while prime[i] * prime[i] <= num and prime[i] != 0:  # 素数リスト内の数で割り切れるか判定
+        counter += 1  # 割り算回数をカウント
+        if num % prime[i] == 0:
+            sosuhantei = 0  # 割り切れれば素数ではない
+            break
+        i += 1  # 次の素数をチェック
+    if sosuhantei == 1:
+        prime[j] = num  # 素数ならリストに追加
+        j += 1  # インデックスを進める
+    num += 1  # 次の数に進む
+
+# 結果表示
+st.write(f"1から{n}までの素数:")
+i = 0
+while prime[i] != 0:
+    st.write(prime[i])
+    i += 1
+
+st.write(f"割り算を行った回数: {counter}")
